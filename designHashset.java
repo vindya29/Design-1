2d67b10c750f536b5f37fb0cdcd9ad61c4630561
vindya <69504272+vindya29@users.noreply.github.com>
2025-03-19 16:14:06 -0500

    Update designHashset.java

--- designHashset.java
+++ designHashset.java
@@ -1,1 +1,63 @@
 
+class MyHashSet {
+
+    boolean[][] storage;
+    int primary;
+    int secondary;
+
+    public MyHashSet() {
+        this.primary=1001;
+        this.secondary=1001;
+        this.storage = new boolean[primary][];
+    }
+    
+
+    private int primaryhash(int key){
+        return key / primary;
+
+    }
+
+    private int secondaryhash(int key){
+        return key%secondary;
+    }
+
+    public void add(int key) {
+        int primaryindex= primaryhash(key);
+
+        if(storage[primaryindex]==null){
+            storage[primaryindex] = new boolean[secondary];
+        }
+
+        int secondaryindex = secondaryhash(key);
+
+        storage[primaryindex][secondaryindex] = true;
+
+        
+    }
+    
+    public void remove(int key) {
+        int primaryindex=primaryhash(key);
+        if(storage[primaryindex]==null){
+            return;
+        }
+
+        int secondaryindex=secondaryhash(key);
+
+        storage[primaryindex][secondaryindex]=false;
+        
+    }
+    
+    public boolean contains(int key) {
+        int primaryindex=primaryhash(key);
+        if(storage[primaryindex]==null){
+            return false;
+        }
+
+        int secondaryindex=secondaryhash(key);
+
+       return storage[primaryindex][secondaryindex];
+        
+    }
+}
+
+
