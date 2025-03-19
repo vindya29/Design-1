e36b1bc73a630aee84f2d70c6cf9072007b985f6
vindya <69504272+vindya29@users.noreply.github.com>
2025-03-19 16:14:33 -0500

    Update minStack.java

--- minStack.java
+++ minStack.java
@@ -1,1 +1,40 @@
 
+
+
+class MinStack {
+
+    Stack<Integer> st;
+    Stack<Integer> minst;
+    int min;
+
+    public MinStack() {
+        
+        this.st=new Stack<>();
+        this.minst=new Stack<>();
+        this.min = Integer.MAX_VALUE;
+        minst.push(min);
+    }
+    
+    public void push(int value) {
+        min = Math.min(min,value);
+        st.push(value);
+        minst.push(min);
+    }
+    
+    public void pop() {
+        st.pop();
+        minst.pop();
+        min = minst.peek();
+        
+    }
+    
+    public int top() {
+        return st.peek();
+        
+    }
+    
+    public int getMin() {
+        
+        return min;
+    }
+}
